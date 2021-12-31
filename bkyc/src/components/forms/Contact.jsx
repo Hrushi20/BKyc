00284c34663e2619f4f64d75db524fbc2219d008
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-01-01 02:31:43 +0530

    Storing form data in react

--- bkyc/src/components/forms/Contact.jsx
+++ bkyc/src/components/forms/Contact.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import contact from '../../assets/suggestions.svg';
 import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade';
 
 const ContactForm = () => {
+
+    const [formSubmitted,setFormSubmitted] = useState(false);
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [message,setMessage] = useState("");
+
+
+    function updateForm(type,value){
+
+      switch(type){
+        case "name":
+          setName(value);
+          break;
+        case "email":
+          setEmail(value);
+          break;
+        case "message":
+          setMessage(value);
+          break;
+        default:
+          console.log("Shouldn't be here");
+      }
+
+    }
+
+    async function submitForm(){
+
+      const body = {
+        name,
+        email,
+        message
+      }
+
+      // Need to create a route to submit the form and store the data in mongoose.
+      
+      // let res = await fetch(`${process.env.REACT_APP_PORTAL}/contact-us`,{
+      //   method:"POST",
+      //   body: JSON.stringify(body)
+      // });
+
+      
+
+      console.log(body);
+
+      setFormSubmitted(true);
+
+    }
+
     return (
             <div className='form'>
                 <div className="text">
@@ -14,12 +62,13 @@ const ContactForm = () => {
                     <img className="contactSvg" src={contact} alt='Write to us' />
                  </Fade>
                 </div>
-                <form className='cform'>
+                
+                {!formSubmitted?<form className='cform'>
                  <Fade right cascade>
                     <div className="inputItem">
                         <label>
                         Name :
-                        <input type="text" name="name" />
+                        <input type="text" name="name" onChange={(e)=>updateForm("name",e.target.value)}/>
                         </label>
                     </div>
                  </Fade>
@@ -27,7 +76,7 @@ const ContactForm = () => {
                     <div className="inputItem">
                         <label>
                         Email :
-                        <input type="text" name="email" />
+                        <input type="text" name="email" onChange={(e)=>updateForm("email",e.target.value)}/>
                         </label>
                     </div>
                   </Fade>
@@ -35,16 +84,27 @@ const ContactForm = () => {
                   <Fade right cascade>
                     <div className="inputItem">
                         <label>
-                        Essay: <br />
-                        <textarea rows={10} cols={34} />
+                        Message: <br />
+                        <textarea rows={10} cols={34} onChange={(e)=>updateForm("message",e.target.value)}/>
                         </label>
                     </div>
                   </Fade>
 
                   <Slide bottom cascade>
-                    <input className='submit' style={{alignSelf: 'center', padding: '8px 44px', borderRadius: 12, backgroundColor: '#ff9933', border: '1px solid gray', color: 'white'}} type="submit" value="Submit" />
+                    <input className='submit' style={{cursor:"pointer" ,alignSelf: 'center', padding: '8px 44px', borderRadius: 12, backgroundColor: '#ff9933', border: '1px solid gray', color: 'white'}} type="submit" value="Submit" onClick={submitForm}/>
                   </Slide>  
-                </form>
+                </form>:
+              
+              <div>
+                <h3>Thank you for submitting your form</h3>
+                <p>Will reach out to you as soon as possible.</p>
+                {/* Lottie file can be added here 
+                
+                  https://iconscout.com/lottie/thank-you-for-signup-4423290
+                  https://iconscout.com/lotties/thank%20you?utm_campaign=search&utm_medium=marketplace&utm_source=lottiefiles
+                */}
+                
+            </div>}
             </div>
     )
 }
