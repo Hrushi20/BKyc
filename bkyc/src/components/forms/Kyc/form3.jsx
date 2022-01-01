1354d624c591fc91851673b5012467f84b1825d6
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-01-01 22:11:41 +0530

    Added live camera feature

--- bkyc/src/components/forms/Kyc/form3.jsx
+++ bkyc/src/components/forms/Kyc/form3.jsx
@@ -2,8 +2,19 @@
 import { Box } from "@mui/system";
 import { TextField } from "@mui/material";
 import contact from "../../../assets/suggestions.svg"
+import Webcam from "react-webcam";
+import { useRef } from "react";
+import { useCallback } from "react";
 
-const Form3 = ({  }) => {
+const Form3 = ({ setLivePhoto,livePhoto }) => {
+
+    const webcamRef = useRef(null);
+
+    const capturePhoto = useCallback(() => {
+
+        setLivePhoto(webcamRef.current.getScreenshot())
+
+    },[setLivePhoto])
 
     return (
         <div style={{ display:"flex",alignItems:"center" }}>
@@ -12,6 +23,17 @@ const Form3 = ({  }) => {
             </div>
             <Box >
                 <input type="file"/>
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                />
+                <button onClick={capturePhoto}>Capture Photo</button>
+                {livePhoto && (
+                    <img
+                    src={livePhoto}
+                    />
+                )}
             </Box>
         </div>
     )
