ede953fc4ed2b316fe38eebadd5b6fe5da0859e5
saimaheshtaduri <saimaheshtaduri6@gmail.com>
2022-01-02 19:46:40 +0530

    status screens are set

--- bkyc/src/views/Profile.jsx
+++ bkyc/src/views/Profile.jsx
@@ -13,35 +13,47 @@ import Form3 from '../components/forms/Kyc/form3';
 import Form2 from '../components/forms/Kyc/form2';
 import Form1 from '../components/forms/Kyc/form1';
 import useKyc from "../hooks/useKyc";
+import Lottie from 'react-lottie';
+import reviewAnim from '../assets/review.json';
+import ethAnim from '../assets/etherium.json';
+
+
+const StepperForm = ({data, activeStep, setActiveStep}) => (
+    <div className="kyc-form" style={{height : activeStep === 3 ? 660 : 450}}>
+              {activeStep === 0? <Form1 {...data}/>:activeStep === 1 ? <Form2 {...data}/>: activeStep === 2 ?<Form3 {...data}/>:<Form4 {...data}/>}
+              <div className="buttons">                
+                <Button variant='outlined' disabled={activeStep === 0} color='inherit' onClick={() => setActiveStep(activeStep - 1)}>Previous</Button>
+                <Button variant='outlined' disabled={activeStep === 3} color='inherit' onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
+              </div>
+    </div>
+)
 
 function Profile() {
 
     const data = useKyc();
     const [activeStep, setActiveStep] = React.useState(0);
-    // const [component, setComponent] = React.useState(<Form1 {...data}/>)
+    const [status, setStatus] = React.useState('verified');
+    const [component, setComponent] = React.useState(<Form1 {...data}/>)
 
     console.log(data);
 
-    // React.useEffect(() => {
-    //     switch (activeStep) {
-    //         case 0:
-    //             setComponent(<Form1 {...data}/>);
-    //             break;
-    //         case 1:
-    //             setComponent(<Form2 {...data}/>);
-    //             break;
-    //         case 2:
-    //             setComponent(<Form3 {...data}/>);
-    //             break;
-    //         case 3:
-    //              setComponent(<Form4 {...data}/>);
-    //              break;
-        
-    //         default:
-    //             break;
-    //     }
-    // }, [activeStep])
+    const review = {
+        loop: true,
+        autoplay: true,
+        animationData: reviewAnim,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
 
+     const etherium = {
+        loop: true,
+        autoplay: true,
+        animationData: ethAnim,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
 
     return (
         <div>
@@ -53,7 +65,7 @@ function Profile() {
                       <Fade left cascade> Profile</Fade>
                     </p>
                     <div className="metadata">
-                        <Chip className='status' color='success' label="Status : verified" />
+                        <Chip className='status' color='success' label= {"status : " + status }  />
                         <Avatar
                         className='avatar'
                         alt="Remy Sharp"
@@ -70,29 +82,52 @@ function Profile() {
 
             <p className="name">Guest</p>
 
+            {
+                status === 'rejected' ? 
+                   <p className="content" style={{fontSize: 15, color: 'red'}} > The previous KYC was rejected. Please fill it again. To know WHY ? <a href='#'> view report </a>  </p>
+                : status === 'verified' ? 
+                     <>
+                        <p className="content"> Congratulations !!. Your KYC is verified.  </p>
+                        <Lottie 
+                            options={etherium}
+                            height={400}
+                            width={400}
+                        /> 
+                        <p className="content"> Know your Hash here - <a href='#'> view Hash </a> </p>
+                     </>
+                : status === 'pending' ? 
+                  <>
+                       <p className="content"> Please Wait !!. Your KYC is under process. </p>
+                      <Lottie 
+                        options={review}
+                        height={400}
+                        width={400}
+                      /> 
+                  </>
+                : <></>
 
-            <Stepper activeStep={activeStep} alternativeLabel>
-                <Step key={0}>
-                    <StepLabel >Fill your personal details</StepLabel>
-                </Step>
-                <Step key={1}>
-                    <StepLabel>Enter your address and phone-number</StepLabel>
-                </Step>
-                <Step key={2}>
-                    <StepLabel >Upload 'AADHAAR' and 'PAN-CARD'</StepLabel>
-                </Step>
-                <Step key={3}>
-                    <StepLabel >Upload Live Photo</StepLabel>
-                </Step>
-            </Stepper>
+            }
 
-            <div className="kyc-form" style={{height : activeStep === 3 ? 660 : 450}}>
-              {activeStep === 0? <Form1 {...data}/>:activeStep === 1 ? <Form2 {...data}/>: activeStep === 2 ?<Form3 {...data}/>:<Form4 {...data}/>}
-              <div className="buttons">                
-                <Button variant='outlined' disabled={activeStep === 0} color='inherit' onClick={() => setActiveStep(activeStep - 1)}>Previous</Button>
-                <Button variant='outlined' disabled={activeStep === 3} color='inherit' onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
-              </div>
-            </div>
+            {(status === 'no KYC' || status === 'rejected') && 
+              <>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    <Step key={0}>
+                        <StepLabel >Fill your personal details</StepLabel>
+                    </Step>
+                    <Step key={1}>
+                        <StepLabel>Enter your address and phone-number</StepLabel>
+                    </Step>
+                    <Step key={2}>
+                        <StepLabel >Upload 'AADHAAR' and 'PAN-CARD'</StepLabel>
+                    </Step>
+                    <Step key={3}>
+                        <StepLabel >Upload Live Photo</StepLabel>
+                    </Step>
+                </Stepper>
+
+                <StepperForm data={data} activeStep={activeStep} setActiveStep={setActiveStep}  />
+              </>
+            }
             
         </div>
     )
