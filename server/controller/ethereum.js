c48735024ea3e5fd31f8a917575ec93c1748876a
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-01-11 13:54:29 +0530

    integrate setting data on ethereum

--- server/controller/ethereum.js
+++ server/controller/ethereum.js
@@ -5,17 +5,16 @@ const { errHandler } = require("../utils/errHandler");
 const getHashedKyc = async(req,res,next) => {
     
     try{
+
         const { userId } = req.body;
-
-        const { cipherKey,ipfsHash } = await UnPaidKycs.findOne({ userId }).exec();
-
+        const { cipherKey,ipfsHash }  = await UnPaidKycs.findOne({ userId }).exec();
         res.status(200).json({ cipherKey,ipfsHash });
     }catch(err){
         errHandler(err,next);
     }
 }
 
-const kycStoredOnBlockchainSuccess = (req,res,next) => {
+const kycStoredOnBlockchainSuccess = async(req,res,next) => {
     try{
 
         const { userId } = req.body;
