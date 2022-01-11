c146bee9ff65447433e876d4064a54f84a384044
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-01-11 17:53:56 +0530

    Fixed twilio errors

--- bkyc/src/views/Profile.jsx
+++ bkyc/src/views/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
--- server/lib/twilio/twilio.js
+++ server/lib/twilio/twilio.js
@@ -1,20 +1,20 @@
 const twilio = require("twilio");
 
+const accountSid = "AC2afd314ab314d7d87c425a0d42a5da47";
+const authToken = "759f0d910924b4709297de30c323fc4f"// Your Auth 
 
+const client = new twilio(accountSid,authToken);
 
 module.exports = class Twilio {
     
-    static accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-    static authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth 
-    static client = new twilio(accountSid, authToken);
-
+     // Your Account SID from www.twilio.com/console
     static async sendMessage(cipherKey,phoneNumber){
         const message = await client.messages
         .create({
             body: `Your kyc has been verified successfully. The cipherKey ${cipherKey} has been used to encrypt your data. 
             Please store the cipherKey safely. Continue payment at http://localhost:3000/profile for uploading your kyc on the blockchain.`,
-            to: phoneNumber, // Text this number
-            messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID
+            to: `+91${phoneNumber}`, // Text this number
+            messagingServiceSid: "MG770eb17b7cecf6396947e755aae76572"
         });
 
         console.log(message.sid);
