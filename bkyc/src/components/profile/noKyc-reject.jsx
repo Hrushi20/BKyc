609e1da120521d82b40ee1be8d66f0497febea56
saimaheshtaduri <saimaheshtaduri6@gmail.com>
2022-01-12 16:01:22 +0530

    location is working

--- bkyc/src/components/profile/noKyc-reject.jsx
+++ bkyc/src/components/profile/noKyc-reject.jsx
@@ -25,7 +25,7 @@ const StepperForm = ({ data, activeStep, setActiveStep }) => {
     }
 
     return (
-        <div className="kyc-form" style={{ height: activeStep === 3 ? 'unset' : 500, padding: activeStep === 3 && 32 }}>
+        <div className="kyc-form" style={{ height: (activeStep === 3 || activeStep === 2) ? 'unset' : 500, padding: (activeStep === 3 || activeStep === 2) && 32 }}>
             {activeStep === 0 ? <Form1 {...data} /> : activeStep === 1 ? <Form2 {...data} /> : activeStep === 2 ? <Form3 {...data} /> : <Form4 {...data} />}
             <div className="buttons">
                 <Button variant='outlined' disabled={activeStep === 0} color='inherit' onClick={() => setActiveStep(activeStep - 1)}>Previous</Button>
