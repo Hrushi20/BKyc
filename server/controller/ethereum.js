3455976eb9118213fab0f6edb1ef90c78c215373
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-01-14 18:20:05 +0530

    refactored and added bank status routes
    Rectored and added userKycAccess routes

--- server/controller/ethereum.js
+++ server/controller/ethereum.js
@@ -5,8 +5,7 @@ const { errHandler } = require("../utils/errHandler");
 const getHashedKyc = async(req,res,next) => {
     
     try{
-
-        const { userId } = req.body;
+        const userId = req.params.userId;
         const { ipfsHash }  = await UnPaidKycs.findOne({ userId }).exec();
         res.status(200).json({ ipfsHash });
     }catch(err){
@@ -17,12 +16,9 @@ const getHashedKyc = async(req,res,next) => {
 const kycStoredOnBlockchainSuccess = async(req,res,next) => {
     try{
 
-        const { userId } = req.body;
-
+        const userId = req.params.userId;
         const { status } = await UserSchema.findOneAndUpdate({ userId },{ status:"verified" },{ new:true }).exec();
-
         await UnPaidKycs.findOneAndDelete({ userId }).exec();
-
         res.status(200).json({ status:status });
 
     }catch(err){
@@ -30,4 +26,16 @@ const kycStoredOnBlockchainSuccess = async(req,res,next) => {
     }
 }
 
-module.exports = { getHashedKyc,kycStoredOnBlockchainSuccess };
+const getKycofUserForBank = (req,res,next) => {
+    try{
+        // Todo
+        //      ====> Neeed to get kyc of user from ethereum blockchain
+        //      ====> Neeed to decrypt the data on server and display the data to the bank...
+        //      ====> Also show the ethereum blockchain response to the user, so that they can believe the data is truly from the blockchain.
+
+    }catch(err){
+        errHandler(err,next);
+    }
+}
+
+module.exports = { getHashedKyc,kycStoredOnBlockchainSuccess,getKycofUserForBank };
