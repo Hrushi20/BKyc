0f40cc97fc46b1da785e566cb87461cd00e2dd9f
saimaheshtaduri <saimaheshtaduri6@gmail.com>
2022-01-19 14:11:47 +0530

    details page updated

--- bkyc/src/components/Item.jsx
+++ bkyc/src/components/Item.jsx
@@ -49,7 +49,7 @@ function Item({data, index, kycVerified,kycRejected, scheduleAMeet}) {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '70vw',
+        width: '88vw',
         bgcolor: 'white',
         border: '1px solid #000',
         borderRadius: 1,
@@ -82,23 +82,23 @@ function Item({data, index, kycVerified,kycRejected, scheduleAMeet}) {
                 onClose={handleClose}
             >
                 <Box sx={style}>
-                    <div className="left-content">
-                        <img src={data.livePhoto} alt="live" width={200} style={{border: '2px solid gray', borderRadius: 10}}  />
+                    <div className="left-content details">
+                        <img src={data.livePhoto} alt="live" width={280} style={{border: '2px solid gray', borderRadius: 10}}  />
                         <div className="detail"><p className='que'>First name : </p> {data.firstName}</div>
                         <div className="detail"><p className='que'>Last name : </p> {data.lastName}</div>
                         <div className="detail"><p className='que'>Middle name : </p> {data.middleName}</div>
                         <div className="detail"><p className='que'>Date-of-Birth : </p> {data.dob}</div>
                         <div className="detail"><p className='que'>Email : </p> {data.email}</div>
+                        <div className="detail"><p className='que'>Phone : </p> {data.phoneNumber}</div>
 
                     </div>
-                    <div className="right-content">
-                        <div className="detail"><p className='que'>Phone : </p> {data.phoneNumber}</div>
+                    <div className="right-content details">
                         <div className="detail"><p className='que'>Address : </p><p style={{width: '60%'}}> {data.address} </p></div>
                         <div className="detail"><p className='que'>Pin : </p> {data.pincode}</div>
                         <div className="detail"><p className='que'>Aadhaar & Pan-card : </p></div>
                         <div style={{display: 'flex', width: '100%',justifyContent: 'space-around'}} className="docs">
-                            <img src={data.aadhar} alt="aadhaar" width={100} style={{border: '2px solid gray', borderRadius: 10}}  />                        
-                            <img src={data.pan} alt="pan" width={100} style={{border: '2px solid gray', borderRadius: 10}}  />                        
+                            <img src={data.aadhar} alt="aadhaar" width={250} style={{border: '2px solid gray', borderRadius: 10}}  />                        
+                            <img src={data.pan} alt="pan" width={250} style={{border: '2px solid gray', borderRadius: 10}}  />                        
                         </div>
                 
                         <Buttons scheduleAMeet={scheduleAMeet} email={data.email} data={data} kycVerified={kycVerified} kycRejected={kycRejected} index={index}/>
--- bkyc/src/components/profile/payment-pending.jsx
+++ bkyc/src/components/profile/payment-pending.jsx
@@ -11,7 +11,7 @@ function DisplayKyc(data) {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '70vw',
+        width: '88vw',
         bgcolor: 'white',
         border: '1px solid #000',
         borderRadius: 1,
@@ -24,21 +24,21 @@ function DisplayKyc(data) {
     return (
         <Box sx={style} >
             <div className="left-content">
-                <img src={data.livePhoto} alt="live" width={200} style={{ border: '2px solid gray', borderRadius: 10 }} />
+                <img src={data.livePhoto} alt="live" width={280} style={{ border: '2px solid gray', borderRadius: 10 }} />
                 <div className="detail"><p className='que'>First name : </p> {data.firstName}</div>
                 <div className="detail"><p className='que'>Last name : </p> {data.lastName}</div>
                 <div className="detail"><p className='que'>Middle name : </p> {data.middleName}</div>
                 <div className="detail"><p className='que'>Date-of-Birth : </p> {data.dob}</div>
                 <div className="detail"><p className='que'>Email : </p> {data.email}</div>
+                <div className="detail"><p className='que'>Phone : </p> {data.phoneNumber}</div>
+                <div className="detail"><p className='que'>Pin : </p> {data.pincode}</div>
             </div>
             <div className="right-content">
-                <div className="detail"><p className='que'>Phone : </p> {data.phoneNumber}</div>
                 <div className="detail"><p className='que'>Address : </p><p style={{ width: '60%' }}> {data.address} </p></div>
-                <div className="detail"><p className='que'>Pin : </p> {data.pincode}</div>
                 <div className="detail"><p className='que'>Aadhaar & Pan-card : </p></div>
                 <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around' }} className="docs">
-                    <img src={data.aadhar} alt="aadhaar" width={100} style={{ border: '2px solid gray', borderRadius: 10 }} />
-                    <img src={data.pan} alt="pan" width={100} style={{ border: '2px solid gray', borderRadius: 10 }} />
+                    <img src={data.aadhar} alt="aadhaar" width={250} style={{ border: '2px solid gray', borderRadius: 10 }} />
+                    <img src={data.pan} alt="pan" width={250} style={{ border: '2px solid gray', borderRadius: 10 }} />
                 </div>
             </div>
         </Box>
